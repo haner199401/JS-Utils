d4ea7e9aa5272e37be80421307215368f284ef6b
haner <haner.du@qq.com>
2015-12-25 11:17:06 +0800

    add

--- tools.js
+++ tools.js
@@ -1,5 +1,11 @@
-var Tools = {
-    renderMsg: function() {
+var Tools = (function(){
+
+    /**
+     * 占位符替换
+     * @returns {*}
+     * var str = 'aa{0}aa{1}bb{2}';
+     */
+ 	function renderMsg() {
         var args = arguments,
             tpl = args[0],
             i = args.length;
@@ -11,10 +17,35 @@ var Tools = {
         }
 
         return tpl;
-    },
-    getParameterByName: function(name){
+    }
+
+
+    /**
+     * 路由 key 获取 value
+     * @param name 路由中 key
+     * @returns {string}
+     * http://www.xxx.com?a=1&b=2
+     */
+    function getParameterByName(name){
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
             results = regex.exec(location.search);
         return results === null ? "" : decodeURIComponent(results[1]);
     }
-}
+
+
+
+
+
+
+
+    /**
+     * 接口
+     * @type {{}}
+     */
+	var api = {};
+
+	api.renderMsg = renderMsg;
+	api.getParameterByName = getParameterByName;
+
+	return api;
+})();
