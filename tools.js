7f1f9016caec88eb31df618d32db4f626c637ae4
haner <haner.du@qq.com>
2015-12-25 11:44:34 +0800

    add method

--- tools.js
+++ tools.js
@@ -1,11 +1,11 @@
-var Tools = (function(){
+var Tools = (function () {
 
     /**
      * 占位符替换
      * @returns {*}
      * var str = 'aa{0}aa{1}bb{2}';
      */
- 	function renderMsg() {
+    function renderMsg() {
         var args = arguments,
             tpl = args[0],
             i = args.length;
@@ -26,26 +26,45 @@ var Tools = (function(){
      * @returns {string}
      * http://www.xxx.com?a=1&b=2
      */
-    function getParameterByName(name){
+    function getParameterByName(name) {
         var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
             results = regex.exec(location.search);
         return results === null ? "" : decodeURIComponent(results[1]);
     }
 
 
-
-
-
+    /**
+     * 动态加载 script 脚本
+     * @param uri
+     * @param callback
+     */
+    function loadScript(uri, callback) {
+        var complete = false;
+        var el = document.createElement("script");
+        el.setAttribute("type", "text/javascript");
+        el.setAttribute("src", uri);
+        if (callback) {
+            el.onload = el.onreadystatechange = function () {
+                var status = this.readyState;
+                if (!complete && (!status || status == "loaded" || status == "complete")) {
+                    complete = true;
+                    callback();
+                }
+            };
+        }
+        document.getElementsByTagName("head")[0].appendChild(el);
+    }
 
 
     /**
      * 接口
      * @type {{}}
      */
-	var api = {};
+    var api = {};
 
-	api.renderMsg = renderMsg;
-	api.getParameterByName = getParameterByName;
+    api.renderMsg = renderMsg;
+    api.getParameterByName = getParameterByName;
+    api.loadScript = loadScript;
 
-	return api;
+    return api;
 })();
